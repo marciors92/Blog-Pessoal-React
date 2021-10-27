627c27fe47e7b96bf6ec2da0f2ddbdfab1bc9b48
unknown <marciors92@gmail.com>
2021-10-27 10:43:40 -0300

    Criando Blog com React

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Home.css';
+
+function Home() {
+    return (
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="https://rachelsquirrel.files.wordpress.com/2019/03/img_6644.jpg" alt="Imagem tela inicial" className="img"/>
+        </>
+    );
+}
+
+export default Home;
