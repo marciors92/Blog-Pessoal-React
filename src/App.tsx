8443620dc0f9244ce6f06d640975179b2f661ded
unknown <marciors92@gmail.com>
2021-11-05 11:38:26 -0300

    Comunicacao com o Back-end

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
 import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
 import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
 import Home from './paginas/home/Home';
-
-
 import './App.css';
 import Login from './paginas/login/login';
+import ListaTema from './components/temas/listatema/listaTema';
+import ListaPostagem from './components/postagens/listapostagem/listaPostagem';
 
 
 function App() {
@@ -23,16 +22,22 @@ function App() {
             <Login />
           </Route>
 
-          <Route path="/login">
+          <Route path='/login'>
             <Login />
           </Route>
 
-          <Route path="/home">
+          <Route path='/home'>
             <Home />
           </Route>
           
-          <Route path="/cadastrousuario">
+          <Route path='/cadastrousuario'>
             <CadastroUsuario />
+          </Route>
+          <Route path='/temas'>
+            <CadastroUsuario />
+          </Route>
+          <Route path='/posts'>
+            <ListaPostagem />
           </Route>
         </div>
       </Switch>
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -8,7 +8,7 @@ function Home() {
             <Grid container direction="row" justifyContent="center" alignItems="center" className='caixa'>
                 <Grid alignItems="center" item xs={6}>
                     <Box paddingX={20} >
-                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className='titulo'>Bem-vindo!</Typography>
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className='titulo'>Bem-vindo! Welcome!</Typography>
                         <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" >Expresse aqui os seus pensamentos</Typography>
                     </Box>
                     <Box display="flex" justifyContent="center">
